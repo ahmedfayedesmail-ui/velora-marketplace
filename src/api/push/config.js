4e12193f0107ad85a9772ccf85475457d78fc22b
ahmedfayedesmail-ui <ahmedfayedesmail@gmail.com>
2026-09-25 06:50:43 +0300

    fix(push): remove stale legacy config endpoint

--- src/api/push/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.status(200).json({
-    enabled: !!process.env.VAPID_PRIVATE_KEY,
-    publicKey: 'BK0OWJTIQ3L62VXZUkoVCyrZkhBSTuajYcusoOcckLId7poLrHYE129EHGh9Kdrb62jrXlCx0rOKtLJv405mfCU'
-  });
-};
